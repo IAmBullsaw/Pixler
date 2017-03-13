416141996c82a351124a149b21aea0c68e913cc4
Oskar Jansson <oskja067@student.liu.se>
2017-03-13 10:13:03 +0100

    Added new test and new test image

--- main.cpp
+++ main.cpp
@@ -5,18 +5,19 @@
 
 void print_vector(std::vector<IPixel*> v) {
   for (IPixel* p : v) {
-    std::cout << *p;
+    std::cout << *p << std::endl;
   }
 }
 
 int main() {
   IParser parser{};
-  std::vector<IPixel*> vr = parser.parse("test_black_1x1.bmp", IParser::BMP);
-  print_vector(vr);
+  //std::vector<IPixel*> vr = parser.parse("test_black_1x1.bmp", IParser::BMP);
+  //print_vector(vr);
   
-  print_vector(parser.parse("test_black_10x10.bmp", IParser::BMP));
+  //print_vector(parser.parse("test_black_10x10.bmp", IParser::BMP));
   
-  print_vector(parser.parse("test_red_green_5x10.bmp", IParser::BMP));  
+  //print_vector(parser.parse("test_red_green_5x10.bmp", IParser::BMP));  
   
+  print_vector(parser.parse("test_RGBW_2x2.bmp", IParser::BMP));
   return 0;
 }
